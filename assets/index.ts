4e2722d914b0fc511fc9b4b77f28f9b47d5bd823
Got-crypto <pecpalmplay@gmail.com>
2024-02-22 23:11:02 +0200

    added logo to header

--- assets/index.ts
+++ assets/index.ts
@@ -1,8 +1,9 @@
 import blurbob from "./blurblob.svg"
+import logo from "./esc.png"
 import logoblob from "./logoblob.svg"
 import rainbow from "./rainbow.svg"
 import silhouette from "./silhouette.jpg"
 import street from "./streets.jpg"
 
-export { blurbob, logoblob, rainbow, silhouette, street }
+export { blurbob, logo, logoblob, rainbow, silhouette, street }
 
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,18 @@
+import { logo } from "@/assets";
+import Image from "next/image";
 
 export default function Header() {
   return (
-    <header className="fixed z-50 h-16 w-full flex flex-col justify-center items-center backdrop-blur-xl shadow-lg shadow-black">
+    <header className="fixed z-50 h-20 w-full flex flex-col justify-center items-center backdrop-blur-xl shadow-lg shadow-black">
         <nav className="h-full w-11/12 flex flex-row justify-between items-center">
-            <p className="text-lg text-primary font-mont">ESC</p>
+            <div className="relative h-20 w-28">
+                <Image
+                    src={logo}
+                    fill
+                    alt="logo"
+                    className="object-contain"
+                />
+            </div>
 
             <div className="w-3/5 h-full flex flex-row justify-between items-center">
                 <ul className="text-white flex flex-row justify-around items-center w-full">
