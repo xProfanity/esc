0b8fe1a06c524b377bfab406491e464c7d0c5d3e
Got-crypto <pecpalmplay@gmail.com>
2024-03-20 22:43:21 +0200

    improving ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 import { Contacts, CoreValues, FundingAndVision, Hero, Missions, Objectives } from "@/components";
+import { FloatingNav } from "@/components/ui/header";
+import { Navlinks } from "@/constants";
 
 export default function Home() {
   return (
     <>
+      <FloatingNav navItems={Navlinks} />
       <Hero />
       <Objectives />
       <Missions />
--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { base } from "@/context/store";
 import {
     AnimatePresence,
     motion,
@@ -7,7 +8,10 @@ import {
 } from "framer-motion";
 import Link from "next/link";
 import { useState } from "react";
+import { useSnapshot } from "valtio";
+import { ThemeButton } from "..";
 import { cn } from "../../lib/utils";
+import Button from "./button";
 
 export const FloatingNav = ({
   navItems,
@@ -16,7 +20,6 @@ export const FloatingNav = ({
   navItems: {
     name: string;
     link: string;
-    icon?: JSX.Element;
   }[];
   className?: string;
 }) => {
@@ -24,8 +27,9 @@ export const FloatingNav = ({
 
   const [visible, setVisible] = useState(false);
 
+  const {darkmode} = useSnapshot(base)
+
   useMotionValueEvent(scrollYProgress, "change", (current) => {
-    // Check if current is not undefined and is a number
     if (typeof current === "number") {
       let direction = current! - scrollYProgress.getPrevious()!;
 
@@ -56,7 +60,7 @@ export const FloatingNav = ({
           duration: 0.2,
         }}
         className={cn(
-          "flex max-w-fit  fixed top-10 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-full dark:bg-black bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] pr-2 pl-8 py-2  items-center justify-center space-x-4",
+          "flex max-w-fit fixed top-10 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded bg-transparent backdrop-blur-sm shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] pr-2 pl-8 py-2  items-center justify-center space-x-4",
           className
         )}
       >
@@ -65,17 +69,17 @@ export const FloatingNav = ({
             key={`link=${idx}`}
             href={navItem.link}
             className={cn(
-              "relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500"
+              "relative items-center flex space-x-1"
             )}
           >
-            <span className="block sm:hidden">{navItem.icon}</span>
-            <span className="hidden sm:block text-sm">{navItem.name}</span>
+            <motion.span animate={{color: darkmode ? '#e3e3e7' : '#011222'}} className="hidden sm:block text-sm hover:text-accent font-semibold font-mont capitalize">{navItem.name}</motion.span>
           </Link>
         ))}
-        <button className="border text-sm font-medium relative border-neutral-200 dark:border-white/[0.2] text-black dark:text-white px-4 py-2 rounded-full">
-          <span>Login</span>
-          <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent  h-px" />
-        </button>
+        <Button>
+            Funding
+        </Button>
+
+        <ThemeButton />
       </motion.div>
     </AnimatePresence>
   );
