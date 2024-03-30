23304c7012c6d99c2a5569bfdfa1ea4d23e11024
Got-crypto <pecpalmplay@gmail.com>
2024-03-30 23:58:33 +0200

    .

--- app/api/funding/pawapay/route.ts
+++ app/api/funding/pawapay/route.ts
@@ -8,5 +8,7 @@ export function GET() {
 export async function POST(req: Request) {
     const body = await req.json()
 
+    if(body.amount === '') return Response.json({error: 'amount required'})
+
     return Response.json({message: `welcome ${body?.fullname}, thanks for the ${body.amount}`})
 }
