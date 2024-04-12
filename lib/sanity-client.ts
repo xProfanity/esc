424f1cbfe6b37d2dce023110c86dadf10b258508
Got-crypto <pecpalmplay@gmail.com>
2024-04-12 12:33:14 +0200

    set useCdn to rails new todolist-with-ruby-on-rails

--- lib/sanity-client.ts
+++ lib/sanity-client.ts
@@ -3,7 +3,7 @@ import { createClient } from "@sanity/client";
 import imageUrlBuilder from '@sanity/image-url';
 
 const client = createClient({
-    useCdn: true,
+    useCdn: false,
     token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECTID,
     apiVersion: '2022-03-07',
@@ -16,3 +16,4 @@ const builder = imageUrlBuilder(client)
 const urlFor = (source: SanityImage) => builder.image(source)
 
 export { client, urlFor };
+
